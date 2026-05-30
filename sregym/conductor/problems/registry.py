834a2db78789eece1a0da3fd652b5a807d6aec53
Muhammad Abdullah (Zaman) <102139203+MABDz@users.noreply.github.com>
2026-05-31 03:38:01 +0500

    New Problem | HPA missing effective CPU request  (#820)
    * Add HPA control-plane oracle
    
    * Support HPA CPU request injection
    
    * Register HPA CPU request problem
    
    * Refactoring Code
    
    * Fix false negatives in HPA diagnosis root cause
    
    * Updated to use standard rollout annotation
    
    ---------
    
    Co-authored-by: Saad Mohammad Rafid Pial <saadmrp222@gmail.com>

--- sregym/conductor/problems/registry.py
+++ sregym/conductor/problems/registry.py
@@ -20,6 +20,7 @@ from sregym.conductor.problems.failed_readiness_probe import FailedReadinessProb
 from sregym.conductor.problems.faulty_image_correlated import FaultyImageCorrelated
 from sregym.conductor.problems.finalizer_deadlock import FinalizerDeadlock
 from sregym.conductor.problems.gc_capacity_degradation import GCCapacityDegradation
+from sregym.conductor.problems.hpa_missing_effective_cpu_request import HPAMissingEffectiveCPURequest
 from sregym.conductor.problems.image_slow_load import ImageSlowLoad
 from sregym.conductor.problems.incorrect_image import IncorrectImage
 from sregym.conductor.problems.incorrect_port_assignment import IncorrectPortAssignment
@@ -150,6 +151,7 @@ class ProblemRegistry:
             "readiness_probe_misconfiguration_social_network": lambda: ReadinessProbeMisconfiguration(app_name="social_network", faulty_service="user-service"),
             "resource_request_too_large": lambda: ResourceRequestTooLarge(app_name="hotel_reservation", faulty_service="mongodb-rate"),
             "resource_request_too_small": lambda: ResourceRequestTooSmall(app_name="hotel_reservation", faulty_service="mongodb-rate"),
+            "hpa_missing_effective_cpu_request_hotel_reservation": lambda: HPAMissingEffectiveCPURequest(),
             "rolling_update_misconfigured_hotel_reservation": lambda: RollingUpdateMisconfigured(app_name="hotel_reservation"),
             "rolling_update_misconfigured_social_network": lambda: RollingUpdateMisconfigured(app_name="social_network"),
             "scale_pod_zero_social_net": ScalePodSocialNet,
