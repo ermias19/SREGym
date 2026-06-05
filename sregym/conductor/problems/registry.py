05e9b1c659634287095d99e8fdd4797adf0fb2b4
ermias19 <ermiasmulu19@gmail.com>
2026-06-05 17:54:42 +0200

    deadlock finiizer running benchmark sesion secured.

--- sregym/conductor/problems/registry.py
+++ sregym/conductor/problems/registry.py
@@ -18,6 +18,7 @@ from sregym.conductor.problems.env_variable_shadowing import EnvVariableShadowin
 from sregym.conductor.problems.failed_readiness_probe import FailedReadinessProbe
 from sregym.conductor.problems.faulty_image_correlated import FaultyImageCorrelated
 from sregym.conductor.problems.finalizer_deadlock import FinalizerDeadlock
+from sregym.conductor.problems.finalizer_deadlock_controller import FinalizerDeadlockController
 from sregym.conductor.problems.gc_capacity_degradation import GCCapacityDegradation
 from sregym.conductor.problems.image_slow_load import ImageSlowLoad
 from sregym.conductor.problems.incorrect_image import IncorrectImage
@@ -117,6 +118,7 @@ class ProblemRegistry:
             "auth_miss_mongodb": MongoDBAuthMissing,
             "configmap_drift_hotel_reservation": lambda: ConfigMapDrift(faulty_service="geo"),
             "finalizer_deadlock_hotel_reservation": FinalizerDeadlock,
+            "finalizer_deadlock_controller_hotel_reservation": FinalizerDeadlockController,
             "duplicate_pvc_mounts_astronomy_shop": lambda: DuplicatePVCMounts(app_name="astronomy_shop", faulty_service="frontend"),
             "duplicate_pvc_mounts_hotel_reservation": lambda: DuplicatePVCMounts(app_name="hotel_reservation", faulty_service="frontend"),
             "duplicate_pvc_mounts_social_network": lambda: DuplicatePVCMounts(app_name="social_network", faulty_service="jaeger"),
